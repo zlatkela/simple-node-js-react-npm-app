452e888ed6b0f868913a6a5ee5a0fe8f0dff3312
zlatkovicm <zlatkovic.milos@gmail.com>
2019-02-27 16:08:12 +0000

    Added jest

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+// });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
 });
