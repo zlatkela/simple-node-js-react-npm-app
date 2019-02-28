643ccc854c5887754ef6b13101a00514697e77b7
zlatkovicm <zlatkovic.milos@gmail.com>
2019-02-28 09:31:58 +0000

    changed jest config

--- jest.config.js
+++ jest.config.js
@@ -182,4 +182,20 @@ module.exports = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
+
+    "roots": [
+        "<rootDir>"
+    ],
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ],
 };
