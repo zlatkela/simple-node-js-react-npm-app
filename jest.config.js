622293f576eb33f99a7719de886053408f79ad1a
zlatkovicm <zlatkovic.milos@gmail.com>
2019-02-28 09:35:50 +0000

    changed jest config transform

--- jest.config.js
+++ jest.config.js
@@ -186,9 +186,6 @@ module.exports = {
     "roots": [
         "<rootDir>"
     ],
-    "transform": {
-        "^.+\\.tsx?$": "ts-jest"
-    },
     "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
     "moduleFileExtensions": [
         "ts",
